d700b5eabc0a1c9aabe9b26f6ca27e52b3ac574d
Samvelo11 <samo.martirosjan@gmail.com>
2024-11-29 09:32:16 +0100

    Add files via upload

---
+++ Lode - zacatek/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lode___zacatek
+{
+    internal class Program
+    {
+        static void arrayPlayerMap(string[] args)
+        {
+            string[,] array = new string[10, 10];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = "-";
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+
+            int[,] array = new int[10, 10];
+            for (int i = 0; i < array.GetLength(0); i++) 
+            {
+                
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = i * 10 + j + 1;
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("pridej lode na souradnice (letadlova lod, bitevni lod, kriznik, ponorka, torpedoborec");
+            Console.WriteLine("p");
+
+                arrayPlayerMap(args);
+
+            Console.ReadKey();
+        }
+    }
+}
