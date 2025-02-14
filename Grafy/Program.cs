652c469ae95b67ca2b1c3fff8f0430bde6aa720a
Samvelo11 <samo.martirosjan@gmail.com>
2025-02-14 09:29:04 +0100

    Add files via upload

---
+++ Grafy/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafy
+{
+    internal class Node
+    {
+        int index;
+        List<Node> neighbours;
+
+        public Node(int index)
+        {
+            this.index = index;
+            neighbours = new List<Node>();
+        }
+
+        public void AddNeighbour(Node neighbour)
+        {
+            if (neighbours.Contains(neighbour))
+            {
+                Console.WriteLine("This neighbour is already present.");
+            }
+            else
+            {
+                neighbours.Add(neighbour);
+            }
+            
+        }
+
+        public void AddNeighbours(List<Node> newNeighbours)
+        {
+            foreach (Node neighbour in newNeighbours)
+            {
+                AddNeighbour(neighbour);
+            }
+            //neighbours.AddRange(newNeighbours);
+        }
+
+        public void PrintNeighbours()
+        {
+            Console.Write("Neighbours of node " + index + ": ");
+            foreach (Node neighbour in neighbours)
+            {
+                Console.Write(neighbour.index + ": ");
+                
+            }
+            Console.WriteLine();
+
+        }
+
+
+    }
+}
